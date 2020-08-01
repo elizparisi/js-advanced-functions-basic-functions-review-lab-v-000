7076dcbc59247f187bb820b954afc348f21f8201
Liz P <parisielizabeth@gmail.com>
2020-08-01 00:40:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Your code here
 
 function saturdayFun(activity="roller-skate"){
-  let activity;
-  console.log("This Saturday, I want to ${activity}");
+  console.log(`This Saturday, I want to ${activity}`);
 }
