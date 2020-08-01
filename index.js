da677d77fbcac9b7bcb851c5533f591b48b09c92
Liz P <parisielizabeth@gmail.com>
2020-08-01 02:52:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ function wrapAdjective(wrapper="*"){
 let Calculator = {
   add: function add(num1, num2){
     return (num1 + num2);
-  }
+  },
   subtract: function subtract(num1, num2){
     return (num1 - num2);
-  }
+  },
   multiply: function multiply(num1, num2){
     return (num1 * num2);
-  }
+  },
   divide: function divide(num1, num2){
     return (num1 / num2);
   }
