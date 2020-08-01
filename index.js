d00363936755d2f4395e865043ffffec822f5d79
Liz P <parisielizabeth@gmail.com>
2020-08-01 02:51:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,4 +18,13 @@ let Calculator = {
   add: function add(num1, num2){
     return (num1 + num2);
   }
+  subtract: function subtract(num1, num2){
+    return (num1 - num2);
+  }
+  multiply: function multiply(num1, num2){
+    return (num1 * num2);
+  }
+  divide: function divide(num1, num2){
+    return (num1 / num2);
+  }
 }
